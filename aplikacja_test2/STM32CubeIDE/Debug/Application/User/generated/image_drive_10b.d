db9715f0e57722ffed366c03e90f6f4bae133347
Patrygie <giepert.patryk@gmail.com>
2023-03-05 18:25:21 +0100

    szkielet panelu jazdy

---
+++ aplikacja_test2/STM32CubeIDE/Debug/Application/User/generated/image_drive_10b.d
@@ -0,0 +1,4 @@
+Application/User/generated/image_drive_10b.o: \
+ D:/TouchGFX/Projekty/aplikacja_test2/TouchGFX/generated/images/src/image_drive_10b.cpp \
+ ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
---
+++ aplikacja_test2/STM32CubeIDE/Debug/Drivers/Components/ft5336.d
@@ -0,0 +1,6 @@
+Drivers/Components/ft5336.o: \
+ D:/TouchGFX/Projekty/aplikacja_test2/Drivers/BSP/Components/ft5336/ft5336.c \
+ D:/TouchGFX/Projekty/aplikacja_test2/Drivers/BSP/Components/ft5336/ft5336.h \
+ d:\touchgfx\projekty\aplikacja_test2\drivers\bsp\components\common\ts.h
+D:/TouchGFX/Projekty/aplikacja_test2/Drivers/BSP/Components/ft5336/ft5336.h:
+d:\touchgfx\projekty\aplikacja_test2\drivers\bsp\components\common\ts.h:
